92ae0a331ea8912f3e24264fc1b4dd6489e798a4
eduardo <eduatrigo@gmail.com>
2024-03-26 13:52:50 -0300

    punteros

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    int *punt_result;
     int a = 1, b=2; 
     printf("HolaMundo\n");
     int result = a + b;
-    printf("suma de %d + %d = %d", a , b , result);
+    punt_result = &result;
+    printf("suma de %d + %d = %d \n", a , b , result);
+    printf("El contenido del puntero %d \n", *punt_result);
+    printf("La direccion de memoria del puntero %p \n", &punt_result);
+    printf("La direccion de memoria de la variable %d \n", &result);
     return 0;
 }
