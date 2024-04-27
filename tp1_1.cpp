39f690a030038df8bc9adff460a67af368fc6f0d
Eduardo Trigo <eduatrigo@gmail.com>
2024-04-27 14:44:29 -0300

    refactor tp1-1

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int *punt_result;
-    int a = 1, b=2; 
+    int a = 1, b = 2;
     printf("HolaMundo\n");
     int result = a + b;
     punt_result = &result;
-    printf("suma de %d + %d = %d \n", a , b , result);
-    printf("El contenido del puntero %d \n", *punt_result);
+    printf("suma de %d + %d = %d \n", a, b, result);
+    printf("El contenido del puntero %d \n", punt_result);
+    printf("El contenido de la variable que apunta el puntero %d \n", *punt_result);
     printf("La direccion de memoria del puntero %p \n", &punt_result);
     printf("La direccion de memoria de la variable %d \n", &result);
+    printf("El tamaño de memoria utilizado por esa variable: %zu bytes\n", sizeof(result));
+
     return 0;
 }
